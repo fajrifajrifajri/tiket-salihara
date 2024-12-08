8d96e019bec3c5d41063cac2d914176ee4a39228
Muhamad Fajri <fajrixmail@gmail.com>
2024-12-09 06:14:33 +0700

    config(package.json): Skipping typescript error (2)

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,16 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import react from '@vitejs/plugin-react';
 import path from 'path';
+import checker from 'vite-plugin-checker';
 
 export default defineConfig({
     plugins: [
         laravel({
             input: 'resources/js/app.tsx',
             refresh: true,
+        }),
+        checker({
+            typescript: false,
         }),
         react(),
     ],

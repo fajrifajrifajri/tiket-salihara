763f93ad25d60085be20d66940dbe1d135cbceb0
Muhamad Fajri <fajrixmail@gmail.com>
2024-12-09 06:01:47 +0700

    deploy(initial-release): First/Earliest trying to deploy to test subdomain, for first testing deployment

--- resources/js/components/slider.tsx
+++ resources/js/components/slider.tsx
@@ -2,27 +2,30 @@ import { cva } from "class-variance-authority";
 
 // Import Swiper React components
 import { Swiper, SwiperSlide } from "swiper/react";
+import { EffectFade } from "swiper/modules";
 
 // Import Swiper styles
 import "swiper/css";
+import "swiper/css/effect-fade";
 
 const sliderStyle = cva(
-    "relative w-full h-[300px] md:h-auto bg-gray-200 flex  justify-center"
+    "relative w-full h-[300px] md:h-auto max-h-[700px] bg-gray-200 flex justify-center"
 );
 
 export default function Slider({ slider }: { slider: any }) {
     return (
         <div className={sliderStyle()}>
             <Swiper
+                modules={[EffectFade]}
+                effect="fade"
                 spaceBetween={50}
-                slidesPerView={3}
+                slidesPerView={1}
                 onSlideChange={() => console.log("slide change")}
                 onSwiper={(swiper) => console.log(swiper)}
             >
                 {slider.map((item: any) => (
-                    <SwiperSlide>
+                    <SwiperSlide key={item.id}>
                         <img
-                            key={item.id}
                             src={item.gambar}
                             alt="Event"
                             className="w-full h-full object-cover"
